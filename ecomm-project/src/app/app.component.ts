7d16a19757d011fb06b2d736ad4db6b52cfe4d0b
Shwetha P G <pgshwetha01@gmail.com>
2024-03-30 19:59:03 +0530

    updated chunk of files

--- ecomm-project/src/app/app.component.ts
+++ ecomm-project/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet,  CommonModule]
 })
 export class AppComponent {
-  title = 'ecomm-project';
+  title = 'TrendBazaar';
 }
