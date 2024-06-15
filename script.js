6d7fac716c497f495b297ba2252566bf8f9ecb01
Shwetha P G <pgshwetha01@gmail.com>
2024-06-15 22:45:59 +0530

    news app base code

---
+++ script.js
@@ -0,0 +1,52 @@
+const apikey = "8b9620907eec4880acee780d7f969bc2";
+const blogContainer = document.getElementById("blog-container");
+
+async function fetchRandomNews() {
+  try {
+    const apiUrl = `https://newsapi.org/v2/top-headlines?sources=techcrunch&pageSize=9&apiKey=${apikey}`;
+    const response = await fetch(apiUrl);
+    const data = await response.json(); // Corrected to response.json()
+    return data.articles;
+  } catch (error) {
+    console.log("Error fetching random news", error);
+    return []; // Return an empty array on error
+  }
+}
+
+function displayBlogs(articles) {
+  blogContainer.innerHTML = "";
+  articles.forEach((article) => {
+    const blogCard = document.createElement("div");
+    blogCard.classList.add("blog-card");
+    const img = document.createElement("img");
+    img.src = article.urlToImage;
+    img.alt = article.title;
+    const title = document.createElement("h2");
+    title.textContent = article.title;
+    const truncateTitle =
+      article.title.length > 30
+        ? article.title.slice(0, 30) + "......"
+        : article.title;
+    title.textContent = truncateTitle;
+    const description = document.createElement("p");
+    const truncatedescription =
+      article.description.length > 120
+        ? article.description.slice(0, 120) + "......"
+        : article.description;
+    description.textContent = truncatedescription;
+
+    blogCard.appendChild(img);
+    blogCard.appendChild(title);
+    blogCard.appendChild(description);
+    blogContainer.appendChild(blogCard);
+  });
+}
+
+(async () => {
+  try {
+    const articles = await fetchRandomNews();
+    displayBlogs(articles);
+  } catch (error) {
+    console.error("Error fetching news", error);
+  }
+})();
